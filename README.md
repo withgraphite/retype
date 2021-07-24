60933167a617315bab012b04ec7817a38fcf1b14
Tomas Reimers <tomasreimers@gmail.com>
2021-07-24 16:31:09 -0400

    README

---
+++ README.md
@@ -0,0 +1,84 @@
+# re-type
+
+Reified types for Typescript.
+
+## About
+
+Re-type gives you a way declare types in a way that the typesystem can read and can also be checked at runtime. The need came from declaring API routes which we wanted to be able to parse for developer experience wins, while still being able to validate at runtime.
+
+## Usage
+
+```typescript
+import * as t from "re-type";
+
+const isBlogPost = t.shape({
+  title: t.string,
+  content: t.string,
+  author: t.optional(
+    t.shape({
+      name: t.string,
+      age: t.number,
+    })
+  ),
+});
+
+type MyValidatorType = t.TypeOf<typeof myValidator>;
+
+isBlogPost({
+  title: "Hello world",
+  content: "Lorem ipsum...",
+}); // returns true
+
+isBlogPost({
+  foo: "bar",
+}); // returns false
+```
+
+## API Reference
+
+### JSON-ifiable Basics
+
+- `t.nulltype`
+- `t.number`
+- `t.string`
+- `t.array(t.string)`
+- `t.shape({memberName: t.string})`
+
+### JavaScript-isms
+
+- `t.undefinedtype`
+- `t.literal("FOO" as const)`
+- `t.tuple([t.string, t.number])`
+- `t.union(t.string, t.number)`
+- `t.intersection(t.shape({name: t.string}), t.shape({age: t.number}))`
+
+### Type relations
+
+- `t.unionMany([t.number, t.string])`
+- `t.intersectMany([t.shape({name: t.string}), t.shape({age: t.number})])`
+- `t.literals(["FOO", "BAR"] as const)`
+
+### Convenience
+
+- `t.optional(t.string)`
+- `t.nullable(t.string)`
+
+## Extension
+
+Adding other validators is very simple, just add a function which returns a type predicate:
+
+```typescript
+export const string = (value: unknown): value is string => {
+  return typeof value === "string";
+};
+```
+
+## Inspiration
+
+Heavily inspired by [io-ts](https://github.com/gcanti/io-ts).
+
+## License
+
+MIT Licensed (see LICENSE.txt)
+
+Copyright 2021, Screenplay Studios Inc.
---
+++ index.ts
@@ -0,0 +1,209 @@
+/**
+ * This library reifies typescript types so that we can validate them at runtime.
+ *
+ * The idea was heavily inspired by https://gcanti.github.io/io-ts/
+ */
+
+export type Schema<TInner> = (value: unknown) => value is TInner;
+
+type ExtractTypeguard<T> = T extends (v: unknown) => v is infer U ? U : never;
+export type TypeOf<A extends Schema<unknown>> = ExtractTypeguard<A>;
+
+export type UnwrapSchemaMap<TSchemaMap> = keyof TSchemaMap extends never
+  ? {
+      [K in any]: undefined;
+    }
+  : {
+      [SchemaMapIndex in keyof TSchemaMap]: TSchemaMap[SchemaMapIndex] extends Schema<unknown>
+        ? TypeOf<TSchemaMap[SchemaMapIndex]>
+        : never;
+    };
+
+export const undefinedtype = (value: unknown): value is undefined => {
+  return typeof value === "undefined";
+};
+
+// Have to clothe the type so that boolean isn't distrubuted
+// I.e. type Extends<A, B> = A extends B ? true : false;
+// results in:
+// Extends<boolean, true> = Extends<true, true> | Extends<false, true> = true | false
+type Extends<A, B> = [A] extends [B] ? true : false;
+type NonGenericExceptBooleans<A> = true extends
+  | Extends<string, A>
+  | Extends<number, A>
+  ? never
+  : A;
+type NonGeneric<A> = true extends Extends<boolean, A>
+  ? never
+  : NonGenericExceptBooleans<A>;
+
+function literalWithoutGenericCheck<TInner>(inner: TInner) {
+  return (value: unknown): value is TInner => {
+    // Not entirely correct, b/c you could have a literal dict, but should work for strings, numbers (might be weird around floats), and booleans
+    return JSON.stringify(inner) === JSON.stringify(value);
+  };
+}
+
+export function literal<TInner>(inner: NonGeneric<TInner>) {
+  return literalWithoutGenericCheck(inner);
+}
+
+// JSON Types
+
+export const number = (value: unknown): value is number => {
+  return typeof value === "number";
+};
+
+export const string = (value: unknown): value is string => {
+  return typeof value === "string";
+};
+
+export const boolean = (value: unknown): value is boolean => {
+  return typeof value === "boolean";
+};
+
+export const nulltype = (value: unknown): value is null => {
+  return value === null;
+};
+
+/**
+ * Note: I'm explicitly excluding dictionaries from this library.
+ * I have seen very few legitimate uses of dictionaries in API design
+ * and more common than not, the use case is better served by a shape
+ * or an array.
+ */
+
+export function shape<
+  TDefnSchema extends {
+    [key: string]: Schema<unknown>;
+  },
+  TDefn extends {
+    [DefnIndex in keyof TDefnSchema]: TypeOf<TDefnSchema[DefnIndex]>;
+  }
+>(schema: TDefnSchema) {
+  return (value: unknown): value is TDefn => {
+    // This explicitly allows additional keys so that the validated object
+    // can be intersected with other shape types (i.e. value is a superset of schema)
+    return (
+      typeof value === "object" &&
+      value !== null && // one of my fave JS-isms: typeof null === "object"
+      Object.keys(schema).every((key: string) => {
+        return schema[key] && schema[key]((value as any)[key]);
+      })
+    );
+  };
+}
+
+export function array<TMember>(member: Schema<TMember>) {
+  return (value: unknown): value is TMember[] => {
+    return (
+      Array.isArray(value) &&
+      value.every((v) => {
+        return member(v);
+      })
+    );
+  };
+}
+
+// Not technically called out in JSON, but we can use JSON for this
+export function tuple<
+  TDefnSchema extends readonly Schema<unknown>[],
+  // Mapped tuple logic derived from https://stackoverflow.com/a/51679156/781199
+  TDefn extends {
+    [DefnIndex in keyof TDefnSchema]: TDefnSchema[DefnIndex] extends Schema<unknown>
+      ? TypeOf<TDefnSchema[DefnIndex]>
+      : never;
+  } & { length: TDefnSchema["length"] }
+>(schema: TDefnSchema) {
+  return (value: unknown): value is TDefn => {
+    return (
+      Array.isArray(value) &&
+      value.length === schema.length &&
+      value.every((v, idx) => {
+        return schema[idx](v);
+      })
+    );
+  };
+}
+
+// Typescript Nonsense
+
+export function union<TLeft, TRight>(
+  left: Schema<TLeft>,
+  right: Schema<TRight>
+) {
+  return (value: unknown): value is TLeft | TRight => {
+    return left(value) || right(value);
+  };
+}
+
+export function intersection<TLeft, TRight>(
+  left: Schema<TLeft>,
+  right: Schema<TRight>
+) {
+  return (value: unknown): value is TLeft & TRight => {
+    return left(value) && right(value);
+  };
+}
+
+export function unionMany<
+  TSchema extends Schema<unknown>,
+  TInnerSchemaType extends TypeOf<TSchema>
+>(inner: TSchema[]) {
+  return (value: unknown): value is TInnerSchemaType => {
+    return inner.some((schema) => {
+      return schema(value);
+    });
+  };
+}
+
+// Check out https://stackoverflow.com/a/50375286/781199 for how we're
+// leveraging distributive conditionals and inference from conditionals
+type TPluralIntersectionType<T> = (
+  T extends Schema<infer U> ? (k: U) => void : never
+) extends (k: infer I) => void
+  ? I
+  : never;
+
+export function intersectMany<
+  TSchema extends Schema<unknown>,
+  TInnerSchemaType extends TPluralIntersectionType<TSchema>
+>(inner: TSchema[]) {
+  return (value: unknown): value is TInnerSchemaType => {
+    return inner.every((schema) => {
+      return schema(value);
+    });
+  };
+}
+
+// Helpers
+
+export function literals<TLiterals>(
+  // See below for why we exclude booleans here
+  inners: readonly NonGenericExceptBooleans<TLiterals>[]
+) {
+  return unionMany(
+    inners.map((value) => {
+      // Note: we intentionally don't use literal(). The reason is that would
+      // break literals([true, false] as const)
+      //
+      // This is because when we call literal() we need to know what type it's being
+      // called with (b/c there is only one lambda in the map), and so logically that
+      // type is inferred as the union of all members of the array. When we pass this function
+      // an array that contains both true and false, the type of literal is infered as
+      // literal<true|false>(value: NonGeneric<true|false>), our mistake catching code in
+      // NonGeneric detects that boolean extends true|false and flags it (assuming you simply
+      // called literal() without as const, e.g. literal(false) errors b/c it gets infered to
+      // literal<boolean>(false))
+      return literalWithoutGenericCheck(value);
+    })
+  );
+}
+
+export function optional<TInner>(inner: Schema<TInner>) {
+  return union(inner, undefinedtype);
+}
+
+export function nullable<TInner>(inner: Schema<TInner>) {
+  return union(inner, nulltype);
+}
