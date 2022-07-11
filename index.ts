f13c02f458a3fb935ec01cc177bca369960a2dac
Jacob Gold <jacob@graphite.dev>
2022-07-11 15:06:04 -0400

    fix: try another way of fixing unwrapschemamap

--- index.ts
+++ index.ts
@@ -15,11 +15,19 @@ export type TypeOf<A extends Schema<unknown>> = ExtractTypeguard<A>;
 
 export type UnwrapSchemaMap<TSchemaMap> = keyof TSchemaMap extends never
   ? Record<string | number | symbol, undefined>
-  : FixOptionalIndices<{
-      [SchemaMapIndex in keyof TSchemaMap]: TSchemaMap[SchemaMapIndex] extends Schema<unknown>
-        ? TypeOf<TSchemaMap[SchemaMapIndex]>
-        : never;
-    }>;
+  :
+      | {
+          [SchemaMapIndex in keyof TSchemaMap]?: TSchemaMap[SchemaMapIndex] extends ReturnType<
+            typeof optional
+          >
+            ? TypeOf<TSchemaMap[SchemaMapIndex]>
+            : never;
+        }
+      | {
+          [SchemaMapIndex in keyof TSchemaMap]: TSchemaMap[SchemaMapIndex] extends Schema<unknown>
+            ? TypeOf<TSchemaMap[SchemaMapIndex]>
+            : never;
+        };
 
 export const undefinedtype = (value: unknown): value is undefined => {
   return typeof value === "undefined";
